ddce12624f17baa70a1b7e4db93b1c5ec4803bba
Marina Jaudy <marinajaudy@MBP-de-Marina.lan>
2024-05-01 15:47:48 -0300

    feat: Create base structure of api, server, eslint, env and other basic files

---
+++ src/env/index.ts
@@ -0,0 +1,17 @@
+import 'dotenv/config'
+import { z } from 'zod'
+
+const envShema = z.object({
+  NODE_ENV: z.enum(['dev', 'test', 'production']).default('dev'),
+  PORT: z.coerce.number().default(3333),
+})
+
+const _env = envShema.safeParse(process.env)
+
+if (_env.success === false) {
+  console.error('❌ Invalid enviroment variables', _env.error.format())
+
+  throw new Error('Invalid enviroment variables.')
+}
+
+export const env = _env.data
